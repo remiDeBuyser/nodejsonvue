24def0ff1783ef6ce8c4974c5a855d0a40dffff4
MyPatoche <remi.de-buyser.auditeur@lecnam.net>
2023-02-06 13:19:57 +0100

    feat: .env

--- bin/socketio.js
+++ bin/socketio.js
@@ -1,11 +1,11 @@
 const request = require('request');
 var cron = require('node-cron');
-
+require('dotenv').config();
 
 var name = "";
 var image = "";
 cron.schedule('*/1 * * * * *', () => {
-    request('https://trefle.io/api/v1/plants?token=QbDbWsykkxWftsD3Tb6ItizBf6S_BA8lv2n5BXLb4XY&filter[image_url]=null', { json: true }, (err, res, body) => {
+    request('https://trefle.io/api/v1/plants?token='+process.env.API_KEY_TREFLE+'&filter[image_url]=null', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         var nb = body.data.length;
         //random number between 0 and nb
