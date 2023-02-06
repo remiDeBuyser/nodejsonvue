9cf2aab2ef814dfee8c2f18445acba640d135470
MyPatoche <remi.de-buyser.auditeur@lecnam.net>
2023-02-06 15:33:33 +0100

    feat: tailwind

--- bin/socketio.js
+++ bin/socketio.js
@@ -4,14 +4,16 @@ require('dotenv').config();
 
 var name = "";
 var image = "";
+var next_page = 1;
 cron.schedule('*/1 * * * * *', () => {
-    request('https://trefle.io/api/v1/plants?token='+process.env.API_KEY_TREFLE+'&filter[image_url]=null', { json: true }, (err, res, body) => {
+    request('https://trefle.io/api/v1/plants?token='+process.env.API_KEY_TREFLE+'&filter_not[image_url]=null&page='+next_page, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         var nb = body.data.length;
         //random number between 0 and nb
         var rand = Math.floor(Math.random() * nb);
         image = body.data[rand].image_url;
-        name = body.data[rand].common_name;
+        name = body.data[rand].scientific_name;
+        next_page = Math.floor(Math.random() * body.links.last.split("page=")[1]);
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,9 @@ const request = require('request');
 var image_url = "";
 var scientific_name = "";
 
-var url = 'https://trefle.io/api/v1/plants?token=QbDbWsykkxWftsD3Tb6ItizBf6S_BA8lv2n5BXLb4XY&page=2';
-
-cron.schedule('*/1 * * * * *', () => {
-  let options = {json: true};
-  request(url, options, (error, res, body) => {
-      if (error) {
-          return  console.log(error)
-      };
-      if (!error && res.statusCode == 200) {
-          var nbdata = body.data.length
-          // random number between 0 and nbdata
-          var random = Math.floor(Math.random() * nbdata);
-          // image_url
-          image_url = body.data[random].image_url;
-          // scientific_name
-          scientific_name = body.data[random].scientific_name;
-      };
-  });
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', image_url: image_url, scientific_name: scientific_name });
+  res.render('index');
 });
 
 module.exports = router;
